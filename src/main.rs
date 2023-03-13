c0b785952dd6d74d57b806e3eeb2b64d96e7905c
Andre <core@uint.dev>
2023-03-13 22:57:02 +0000

    Avoid unwrap for regex

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn file_process(file_handle: &File) -> io::Result<Vec<String>> {
     // Pattern courtesy of https://emailregex.com/
     let email_regex = Regex::new(
         r#"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#
-    ).unwrap();
+    ).expect("Malformed regex pattern");
 
     let mut email_addresses: Vec<String> = Vec::new();
 
